c90eb1520ce5b5ae02c7c6d144015b0ea174520f
Alex <alexanderthestudent@yandex.ru>
2026-09-16 09:29:53 +0300

    init

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Light Android dev shell, this version is LLM-generated";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config = {
+        allowUnfree = true;
+        android_sdk.accept_license = true;
+      };
+    };
+
+    android = pkgs.androidenv.composeAndroidPackages {
+      buildToolsVersions = [ "34.0.0" ];
+      platformVersions = [ "34" ];
+      includeNDK = false;
+      includeEmulator = false;
+    };
+
+    androidSdk = android.androidsdk;
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = [
+        androidSdk
+        pkgs.jdk17
+      ];
+
+      shellHook = ''
+        export ANDROID_HOME=${androidSdk}/libexec/android-sdk
+        export ANDROID_SDK_ROOT=$ANDROID_HOME
+        export JAVA_HOME=${pkgs.jdk17}
+        export PATH=$ANDROID_HOME/platform-tools:$PATH
+      '';
+    };
+  };
+}
+
