042ce88b7dc5bfd561f86cf7ce0a174f3cbc5683
Alex <alexanderthestudent@yandex.ru>
2026-09-17 13:18:57 +0300

    feat: replaceApp method

--- src/main/java/net/snlx/backbone/MainActivity.kt
+++ src/main/java/net/snlx/backbone/MainActivity.kt
@@ -5,9 +5,12 @@ import android.os.Bundle
 import android.widget.TextView
 import android.widget.Toast
 import android.webkit.WebView
+import android.webkit.WebSettings
 import android.webkit.WebViewClient
 import android.webkit.JavascriptInterface
 import android.content.Context
+
+val DEFAULT_URL = "http://192.168.50.174:8899"
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,15 +19,29 @@ class MainActivity : Activity() {
         val webview = WebView(this)
         setContentView(webview)
         webview.settings.userAgentString = "backbone"
+        webview.settings.cacheMode = WebSettings.LOAD_NO_CACHE
         webview.settings.javaScriptEnabled = true
-        webview.addJavascriptInterface(System(this), "system")
-        webview.loadUrl("data:text/html;utf-8,<h1>Hi android</h1><script>system.showToast('hi')</script>")
+        webview.addJavascriptInterface(System(this), "backbone")
+
+        val pref = this.getPreferences(Context.MODE_PRIVATE) ?: return
+        val url = pref.getString("url", DEFAULT_URL).toString()
+        webview.loadUrl(url)
     }
 }
 
-class System(private val mContext: Context) {
+class System(private val app: MainActivity) {
     @JavascriptInterface
     fun showToast(message: String) {
-        Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+        Toast.makeText(app, message, Toast.LENGTH_SHORT).show()
+    }
+
+    @JavascriptInterface
+    fun replaceApp(url: String) {
+        val pref = app.getPreferences(Context.MODE_PRIVATE) ?: return
+
+        with (pref.edit()) {
+            putString("url", url)
+            apply()
+        }
     }
 }
